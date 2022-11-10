7645e64df8b28ddcf1cb465ac4789dfb63dd95d5
Goncharov Sergey <101650540+SErsH69@users.noreply.github.com>
2022-11-10 08:52:22 +0500

    Update header.php

--- header.php
+++ header.php
@@ -46,7 +46,6 @@ $site_address = get_option('site_address');
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="robots" content="noindex, nofollow" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?php echo get_theme_file_uri(); ?>/style.css">
     <?php wp_head(); ?>
